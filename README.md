f5b8e7047137f3767de2199a5d3b822aca610f07
dimitri <dimi309@users.noreply.github.com>
2017-11-26 12:13:19 +0100

    Prepared v1.8.0

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [ ![Download](https://api.bintray.com/packages/bincrafters/public-conan/gtest%3Abincrafters/images/download.svg) ](https://bintray.com/bincrafters/public-conan/gtest%3Abincrafters/_latestVersion)
-[![Build Status](https://travis-ci.org/bincrafters/conan-gtest.svg?branch=stable%2F1.7.0)](https://travis-ci.org/bincrafters/conan-gtest)
+[![Build Status](https://travis-ci.org/bincrafters/conan-gtest.svg?branch=stable%2F1.8.0)](https://travis-ci.org/bincrafters/conan-gtest)
 [![Build status](https://ci.appveyor.com/api/projects/status/sxs9n6vb8nqa92l5?svg=true)](https://ci.appveyor.com/project/BinCrafters/conan-gtest)
 
 [Conan.io](https://conan.io) package for [gtest](https://github.com/google/googletest) project
@@ -10,14 +10,14 @@ The packages generated with this **conanfile** can be found in [Bintray](https:/
 
 ### Basic setup
 
-    $ conan install gtest/1.7.0@bincrafters/stable
+    $ conan install gtest/1.8.0@bincrafters/stable
 
 ### Project setup
 
 If you handle multiple dependencies in your project is better to add a *conanfile.txt*
 
     [requires]
-    gtest/1.7.0@bincrafters/stable
+    gtest/1.8.0@bincrafters/stable
 
     [generators]
     cmake
@@ -44,7 +44,7 @@ The following command both runs all the steps of the conan file, and publishes t
 
 ## Upload
 
-    $ conan upload gtest/1.7.0@bincrafters/stable --all -r bincrafters
+    $ conan upload gtest/1.8.0@bincrafters/stable --all -r bincrafters
 
 ## License
-[BSD 3-Clause](https://github.com/google/googletest/blob/c99458533a9b4c743ed51537e25989ea55944908/LICENSE)
+[BSD 3-Clause](https://github.com/google/googletest/blob/master/googletest/LICENSE)
--- conanfile.py
+++ conanfile.py
@@ -6,15 +6,18 @@ from conans import CMake
 
 class GTestConan(ConanFile):
     name = "gtest"
-    version = "1.7.0"
+    version = "1.8.0"
     sources_folder = "sources"
     generators = "cmake"
     settings = "os", "arch", "compiler", "build_type"
-    options = {"shared": [True, False]}
-    default_options = "shared=True"
-    exports = ["CMakeLists.txt", "LICENSE"]
-    url="http://github.com/lasote/conan-gtest"
+    options = {"shared": [True, False], "cygwin_msvc": [True, False],
+               "no_gmock": [True, False], "no_main": [True, False], "fpic": [True, False]}
+    default_options = ("shared=True", "cygwin_msvc=False", "no_gmock=False",
+                       "no_main=False", "fpic=False")
+    exports = "CMakeLists.txt"
+    url="http://github.com/bincrafters/conan-gtest"
     license="https://github.com/google/googletest/blob/master/googletest/LICENSE"
+    description = "Google's C++ test framework"
     
     def source(self):
         zip_name = "release-%s.zip" % self.version
@@ -27,25 +30,31 @@ class GTestConan(ConanFile):
 
     def build(self):
         cmake = CMake(self)
-        cmake.definitions['BUILD_SHARED_LIBS'] = self.options.shared
-        cmake.definitions['gtest_force_shared_crt'] = True
+        if self.settings.compiler == "Visual Studio" and "MD" in str(self.settings.compiler.runtime):
+            cmake.definitions["gtest_force_shared_crt"] = True
+        cmake.definitions["BUILD_SHARED_LIBS"] = self.options.shared
+        cmake.definitions["CMAKE_POSITION_INDEPENDENT_CODE"] = self.options.fpic
+        cmake.definitions["BUILD_GTEST"] = self.options.no_gmock
+        cmake.definitions["BUILD_GMOCK"] = not self.options.no_gmock
         cmake.configure()
         cmake.build()
 
     def package(self):
+
+        # Copy the license files
+        self.copy("license*", src="%s/googletest" % self.sources_folder, dst=".", ignore_case=True, keep_path=False)
         # Copying headers
-        self.copy(pattern="*.h", dst="include", src="%s/include" % self.sources_folder, keep_path=True)
+        self.copy(pattern="*.h", dst="include", src="%s/googletest/include" % self.sources_folder, keep_path=True)
+        if not self.options.no_gmock:
+            self.copy(pattern="*.h", dst="include", src="%s/googlemock/include" % self.sources_folder, keep_path=True)
 
         # Copying static and dynamic libs
         self.copy(pattern="*.a", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.lib", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.dll", dst="bin", src=".", keep_path=False)
         self.copy(pattern="*.so*", dst="lib", src=".", keep_path=False)
-        self.copy(pattern="*.dylib*", dst="lib", src=".", keep_path=False)      
+        self.copy(pattern="*.dylib*", dst="lib", src=".", keep_path=False)
         self.copy(pattern="*.pdb", dst="lib", src=".", keep_path=False)
-
-        # Copying the license
-        self.copy("LICENSE*", dst="licenses",  ignore_case=True, keep_path=False)
 
     def package_info(self):
         self.cpp_info.libs = ['gtest', 'gtest_main']
