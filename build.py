43622dbd9b84d25dbe631225c578a2853e960543
Ovidiu-Florin BOGDAN <ovidiub13@users.noreply.github.com>
2018-04-11 14:20:29 +0300

    Add describing comment

--- build.py
+++ build.py
@@ -5,7 +5,7 @@
 from bincrafters import build_template_default
 
 if __name__ == "__main__":
-
+    # Set True when the target is NOT pure C project
     pure_c = False
     builder = build_template_default.get_builder(pure_c=pure_c)
     builder.run()
