3711b63ba88da371913377d71b0d1b5445f8676c
Uilian Ries <uilianries@gmail.com>
2018-09-02 18:11:57 -0300

    Use own tuple when MSVC < 12
    Signed-off-by: Uilian Ries <uilianries@gmail.com>

--- conanfile.py
+++ conanfile.py
@@ -81,6 +81,10 @@ class GTestConan(ConanFile):
             if self.settings.compiler == "Visual Studio" and self.settings.compiler.version == "11":
                 self.cpp_info.defines.append('_VARIADIC_MAX=10')
 
-        if self.settings.compiler == "Visual Studio" and float(str(self.settings.compiler.version)) >= 15:
-            self.cpp_info.defines.append("GTEST_LANG_CXX11=1")
-            self.cpp_info.defines.append("GTEST_HAS_TR1_TUPLE=0")
+        if self.settings.compiler == "Visual Studio":
+            if float(str(self.settings.compiler.version)) >= 15:
+                self.cpp_info.defines.append("GTEST_LANG_CXX11=1")
+                self.cpp_info.defines.append("GTEST_HAS_TR1_TUPLE=0")
+            elif float(str(self.settings.compiler.version)) <= 12:
+                self.cpp_info.defines.append("GTEST_USE_OWN_TR1_TUPLE=1")
+
